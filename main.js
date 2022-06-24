57e7fe1403c9ade2cc9dd6ebcae99b4d5c354d13
hnamneit <107628763+hnamneit@users.noreply.github.com>
2022-06-24 13:10:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const $ = document.querySelector.bind(document)
 const sidebar = $('.sidebar')
+const nav_menu = $('.nav_icon ul')
+const nav_bar = $('.nav_icon')
+const nav_close_icon = $('.fa-xmark')
+const nav_bar_icon = $('.fa-bars')
+
 document.onscroll = () => {
     const scrollTop = document.documentElement.scrollTop || window.scrollY;
     const documentWidth = document.body.scrollWidth
@@ -7,4 +12,19 @@ document.onscroll = () => {
         console.log(sidebar.offsetWidth-scrollTop/sidebar.offsetWidth)
         sidebar.style.opacity = (sidebar.offsetWidth-scrollTop)/sidebar.offsetWidth
     }
+}
+
+nav_bar_icon.onclick = () => {
+    nav_menu.style.transform = 'translateX(0)'
+    nav_bar_icon.style.display = 'none'
+    nav_close_icon.style.display = 'block'
+    
+
+
+}
+
+nav_close_icon.onclick = () => {
+    nav_menu.style.transform = 'translateX(100%)'
+    nav_bar_icon.style.display = 'block'
+    nav_close_icon.style.display = 'none'
 }
